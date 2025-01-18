97ec019e97f09d36400ead064f07aea70dd8972f
benjamon7143 <103152807+benjamon7143@users.noreply.github.com>
2025-01-18 05:43:15 -0300

    se elimina console.log del tiempo

--- js/script.js
+++ js/script.js
@@ -11,8 +11,8 @@ document.addEventListener('DOMContentLoaded', () => {
   function updateCountdown() {
     const now = new Date();
     const timeDifference = eventDate - now;
-
-    console.log("Time difference:", timeDifference);  // Muestra la diferencia de tiempo
+    
+    // Muestra la diferencia de tiempo
 
     if (timeDifference > 0) {
       const days = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
